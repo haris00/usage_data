2ebfa614927f410424cf2dfab34a6fdae572a836
haris tanvir <haris.tanvir@woodplc.com>
2019-10-07 13:57:30 -0700

    initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Usage Data Uplight',
+      author='haris tanvir'
+      # packages=['distutils', 'distutils.command'],
+      )
