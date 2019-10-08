698bbdde77b61477e3e20a598f673a013772f242
haris tanvir <haris.tanvir@woodplc.com>
2019-10-07 18:54:48 -0700

    extract data from id functionality initial pass

--- tests/test_data_usage_app.py
+++ tests/test_data_usage_app.py
@@ -7,18 +7,19 @@ from usage_data.lib import extract_data_from_id, peak_usage, format_peak_usage, 
 class UsageData(unittest.TestCase):
 
     def test_extract_data_from_id(self):
-        hours = list(range(0, 23))
-        kwhs_usage = [4.898962, 4.725122, 5.889937, 7.055993, 8.147902, 9.526529, 9.654547, 9.497643, 7.656154, 6.922413
+        hours = tuple(range(0, 24))
+        kwhs_usage = (6.439924, 4.898962, 4.725122, 5.889937, 7.055993, 8.147902, 9.526529, 9.654547, 9.497643, 7.656154, 6.922413
                       , 4.821351, 5.117135, 6.103418, 7.221611, 10.372388, 12.87602, 15.43456, 18.041841, 18.20572,
-                      16.7765, 15.114921, 12.12373, 8.830045]
-        extracted_hours, extracted_kwhs_usage = extract_data_from_id('white_house', 'test_data/example_homes_data', csv)
+                      16.7765, 15.114921, 12.12373, 8.830045)
+        extracted_hours, extracted_kwhs_usage = extract_data_from_id('white_house', 'test_data/example_homes_data.csv'
+                                                                     , csv)
         self.assertEqual((extracted_hours, extracted_kwhs_usage), (hours, kwhs_usage))
 
     def test_peak_usage(self):
-        hours = list(range(0, 23))
-        kwhs_usage = [4.898962, 4.725122, 5.889937, 7.055993, 8.147902, 9.526529, 9.654547, 9.497643, 7.656154, 6.922413
+        hours = tuple(range(0, 24))
+        kwhs_usage = (4.898962, 4.725122, 5.889937, 7.055993, 8.147902, 9.526529, 9.654547, 9.497643, 7.656154, 6.922413
                       , 4.821351, 5.117135, 6.103418, 7.221611, 10.372388, 12.87602, 15.43456, 18.041841, 18.20572,
-                      16.7765, 15.114921, 12.12373, 8.830045]
+                      16.7765, 15.114921, 12.12373, 8.830045)
         self.assertEqual(peak_usage(hours, kwhs_usage), (18, 18.20572))
 
     def test_format_peak_usage(self):
@@ -27,10 +28,10 @@ class UsageData(unittest.TestCase):
         self.assertEqual(format_peak_usage(time, kwh_usage), "75.8 kWh at 13:00")
 
     def test_expected_savings(self):
-        hours = list(range(0, 23))
-        kwhs_usage = [4.898962, 4.725122, 5.889937, 7.055993, 8.147902, 9.526529, 9.654547, 9.497643, 7.656154, 6.922413
+        hours = tuple(range(0, 24))
+        kwhs_usage = (4.898962, 4.725122, 5.889937, 7.055993, 8.147902, 9.526529, 9.654547, 9.497643, 7.656154, 6.922413
                       , 4.821351, 5.117135, 6.103418, 7.221611, 10.372388, 12.87602, 15.43456, 18.041841, 18.20572,
-                      16.7765, 15.114921, 12.12373, 8.830045]
+                      16.7765, 15.114921, 12.12373, 8.830045)
 
         self.assertEqual(expected_savings(hours, kwhs_usage, 30, (12, 18)), 26.476667399999997)
 
--- usage_data/lib.py
+++ usage_data/lib.py
@@ -1,9 +1,24 @@
 def extract_data_from_id(id, file_name, csv):
-    return (None, None)
+    with open(file_name) as usage_data:
+        csv_reader = csv.reader(usage_data, delimiter=',')
+        # to skip the header
+        next(csv_reader)
+        hours = []
+        kwhs_usage = []
+        for row in csv_reader:
+            if str(row[0]) == str(id):
+                hours.append(int(row[1]))
+                kwhs_usage.append(float(row[2]))
+    hours_kwh_usage_bundle = list(zip(hours, kwhs_usage))
+    hours_kwh_usage_bundle.sort(key=lambda x: x[0])
+    hours, kwhs_usage = zip(*hours_kwh_usage_bundle)
+    return tuple(hours), tuple(kwhs_usage)
 
 
 def peak_usage(hours, kwhs_usage):
-    """Returns peak kwh usage and associated hour"""
+    """Returns peak kwh usage and associated hour
+    Note: hours and kwhs are assumed sorted
+    """
     max_kwh_usage = max(kwhs_usage)
     # Note if there are multiple same values, the one with the lower index is used
     max_index = kwhs_usage.index(max_kwh_usage)
@@ -19,7 +34,9 @@ def format_peak_usage(time, kwh_usage):
 
 
 def expected_savings(hours, kwhs_usage, percentage_saving, period):
-    """Returns expected savings in kwh and associated hour"""
+    """Returns expected savings in kwh and associated hour
+    Note: hours and kwhs are assumed sorted
+    """
     saving_starting_index = hours.index(period[0])
     saving_ending_index = hours.index(period[1])
     kwh_usage_in_period = sum(kwhs_usage[saving_starting_index: saving_ending_index + 1])
